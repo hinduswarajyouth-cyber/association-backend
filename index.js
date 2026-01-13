ef8e99d876fe6695582c593398e3976b07646105
Hinduswaraj <hinduswarajyouth@gmail.com>
2026-01-14 02:00:57 +0530

    member

--- index.js
+++ index.js
@@ -48,7 +48,12 @@ app.use(
        return callback(null, true);
     },
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+  "Content-Type",
+  "Authorization",
+  "Cache-Control",
+  "Pragma"
+],
     credentials: true,
   })
 );
